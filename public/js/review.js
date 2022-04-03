bf5802f41a8eb9e07609c30f118ffc303278fd1c
Mohamed <spider_mo@hotmail.nl>
2022-04-03 15:29:47 +0200

    last fix to enhancement

--- public/js/review.js
+++ public/js/review.js
@@ -1,6 +1,9 @@
 const reviews = document.getElementsByClassName("review");
 const spots = document.getElementsByClassName("addSpot");
 
+const reviewsLabel = document.querySelectorAll(".submit-review-label");
+const spotsLabel = document.querySelectorAll(".submit-spot-label");
+
 for (let i = 0; i < reviews.length; i++) {
   reviews[i].addEventListener("submit", addReviewText);
 }
@@ -8,6 +11,14 @@ for (let i = 0; i < reviews.length; i++) {
 for (let i = 0; i < spots.length; i++) {
   spots[i].addEventListener("submit", addSpot);
 }
+
+reviewsLabel.forEach((review) => {
+  review.style.display = "none";
+});
+
+spotsLabel.forEach((spot) => {
+  spot.style.display = "none";
+});
 
 document.getElementById("submit-text-return").addEventListener("click", () => {
   document.getElementById("submit-text").style.display = "none";
